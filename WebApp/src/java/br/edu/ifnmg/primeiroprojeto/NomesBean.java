349e87f4b665eb0a23e825ccf078248ead9185bd
PaulohSouza <souza.phn.agro@gmail.com>
2020-04-21 16:34:27 -0400

    Ola nome sobrenome

--- WebApp/src/java/br/edu/ifnmg/primeiroprojeto/NomesBean.java
+++ WebApp/src/java/br/edu/ifnmg/primeiroprojeto/NomesBean.java
@@ -7,5 +7,39 @@ import javax.faces.bean.ManagedBean;
 @SessionScoped
 @ManagedBean
 public class NomesBean implements Serializable{
+    private String nome;
+    private String Sobrenome;
+    private String mensagem;
+    
+    public void dizerOla(){
+        mensagem = "Olá" + nome + " " + Sobrenome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return Sobrenome;
+    }
+
+    public void setSobrenome(String Sobrenome) {
+        this.Sobrenome = Sobrenome;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+    
     
 }
+
+
