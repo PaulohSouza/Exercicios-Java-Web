1139298ce48d19bad8703b38e6492cc7d9055dfc
PaulohSouza <souza.phn.agro@gmail.com>
2020-04-21 16:09:12 -0400

    Curso

---
+++ WebApp/src/java/br/edu/ifnmg/primeiroprojeto/NomesBean.java
@@ -0,0 +1,11 @@
+package br.edu.ifnmg.primeiroprojeto;
+
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.bean.ManagedBean;
+
+@SessionScoped
+@ManagedBean
+public class NomesBean implements Serializable{
+    
+}
